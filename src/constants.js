7d4834a3f2d542986d3e614be0db0b8bf517bc2f
CarvedCoder <shreyansbehera84@gmail.com>
2025-04-15 00:26:57 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -13,7 +13,7 @@ export const dialogueData = {
     Contact me at shreyansbehera84@gmail.com if you have any interesting job opportunities!`,
     projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
     Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. It's a whole separate box lol.
-    Very big thanks to freecodecamp for the tutorial and the map (it saved me a lot of time and hassle).`,
+    Very big thanks to freecodecamp for the map (it saved me a lot of time and hassle).`,
     library: `There are a lot of programming books on my shelves.
     I probably only read one or two of them. I just compulsively buy technical books without ever finishing them XD`,
     exit: `If you want to exit CarvedCoder's portfolio, just close the tab.`,
