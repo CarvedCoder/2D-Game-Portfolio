26549bb56108a4b10b786d00143afc5ff87e4e7e
CarvedCoder <shreyansbehera84@gmail.com>
2025-04-15 00:13:46 +0530

    My personal Game Portfolio

---
+++ src/constants.js
@@ -0,0 +1,20 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `This is where I work. I am a first year student (currently) and i can use multiple languages.
+      And I like to use them to make fun small projects like this website !!! . I mostly use python and c++ nowadays since I'm a data scientist but anyways regardless of the language, I just like programming.
+      Here is my <a href="https://github.com/CarvedCoder" target="_blank">Github</a>!`,
+    "cs-degree": `This is my CS degree (which I'm going to get in three years).I will miss these days very much`,
+    "sofa-table": `That's my sofa. I like to relax here and watch YouTube or IG reels . 
+    I also make gaming videos on YouTube. You can sub to <a href="https://www.youtube.com/@CarvedGamerYT" target="_blank">my channel</a>! (If you like the content)`,
+    tv: `That's my TV. I've been watching tech and programming youtubers a lot recently`,
+    bed: `This where I sleep. I have a very bad sleep schedule but I've been trying to improve it.I also get most of my ideas when I'm trying to sleep`,
+    resume: `This is my desk and on it is my resume. <a href="" target="_blank">Check it out?</a>
+    Contact me at shreyansbehera84@gmail.com if you have any interesting job opportunities!`,
+    projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+    Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. It's a whole separate box lol.
+    Very big thanks to freecodecamp for the tutorial and the map (it saved me a lot of time and hassle).`,
+    library: `There are a lot of programming books on my shelves.
+    I probably only read one or two of them. I just compulsively buy technical books without ever finishing them XD`,
+    exit: `If you want to exit CarvedCoder's portfolio, just close the tab.`,
+  };
